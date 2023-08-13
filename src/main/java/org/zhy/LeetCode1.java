b4ff06a2913d829faebec95fc010157d31a16268
ColinZhy <384414328@qq.com>
2023-08-13 10:16:01 +0800

    pull test

---
+++ src/main/java/org/zhy/LeetCode1.java
@@ -0,0 +1,30 @@
+package org.zhy;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ */
+public class LeetCode1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new NullPointerException("没有符合值");
+    }
+}
