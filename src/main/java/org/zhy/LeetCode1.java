96610fcabf8f7cce83d09d66f8b8ada30b5c95f3
ColinZhy <384414328@qq.com>
2023-08-13 10:26:16 +0800

    LeetCode1 两数之和

--- src/main/java/org/zhy/LeetCode1.java
+++ src/main/java/org/zhy/LeetCode1.java
@@ -1,5 +1,7 @@
 package org.zhy;
 
+import java.util.HashMap;
+
 /**
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
@@ -7,15 +9,8 @@ package org.zhy;
  */
 public class LeetCode1 {
 
-    public static void main(String[] args) {
-
-    }
-
     /**
-     *
-     * @param nums
-     * @param target
-     * @return
+     * 双重循环
      */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -23,6 +18,21 @@ public class LeetCode1 {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
+            }
+        }
+        throw new NullPointerException("没有符合值");
+    }
+
+    /**
+     * 利用Map
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> temp = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (temp.containsKey(target - nums[i])) {
+                return new int[]{i, temp.get(target - nums[i])};
+            } else {
+                temp.put(nums[i], i);
             }
         }
         throw new NullPointerException("没有符合值");
