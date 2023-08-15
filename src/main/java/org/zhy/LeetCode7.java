ff5162d61cdef9a48d80ae250b2e35c1f496ac4e
ColinZhy <384414328@qq.com>
2023-08-15 21:42:46 +0800

    LeetCode7 整数反转

---
+++ src/main/java/org/zhy/LeetCode7.java
@@ -0,0 +1,35 @@
+package org.zhy;
+
+/**
+ * 7. 整数反转
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−2^31,  2^31 − 1] ，就返回 0。
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ */
+public class LeetCode7 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MIN_VALUE / 10);
+        System.out.println(Integer.MAX_VALUE / 10);
+    }
+
+    public int reverse(int x) {
+        int result = 0;
+        int tmp = 0;
+        while (x != 0) {
+            tmp = x % 10;
+            //判断溢出（是否大于最大值 2 ^ 31 - 1 = 2147483647 或者 小于最大值 -2 ^ 31 = -2147483648）
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && tmp > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && tmp < -8)) {
+                return 0;
+            }
+            result = result * 10 + tmp;
+            x = x / 10;
+
+        }
+        return result;
+    }
+}
