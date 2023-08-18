e4ae7de07f96f9ed7232cce596723bd0e9e15476
ColinZhy <384414328@qq.com>
2023-08-18 21:39:35 +0800

    LeetCode15 三数之和

--- src/main/java/org/zhy/LeetCode15.java
+++ src/main/java/org/zhy/LeetCode15.java
@@ -20,18 +20,18 @@ public class LeetCode15 {
         for (int i = 0; i < len; i++) {
             if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
             if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
-            int L = i + 1;
-            int R = len - 1;
-            while (L < R) {
-                int sum = nums[i] + nums[L] + nums[R];
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[L], nums[R]));
-                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
-                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
-                    L++;
-                    R--;
-                } else if (sum < 0) L++;
-                else if (sum > 0) R--;
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++; // 去重
+                    while (left < right && nums[right] == nums[right - 1]) right--; // 去重
+                    left++;
+                    right--;
+                } else if (sum < 0) left++;
+                else right--;
             }
         }
         return result;
