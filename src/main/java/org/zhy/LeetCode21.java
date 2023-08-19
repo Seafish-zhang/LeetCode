00f11266274a076cc493841b978036813a048ed9
ColinZhy <384414328@qq.com>
2023-08-19 15:23:32 +0800

    LeetCode21 合并两个有序链表

---
+++ src/main/java/org/zhy/LeetCode21.java
@@ -0,0 +1,47 @@
+package org.zhy;
+
+import org.zhy.structure.ListNode;
+
+/**
+ * 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+public class LeetCode21 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode head = null;
+        if (list1.val > list2.val) {
+            head = list2;
+            list2 = list2.next;
+        } else {
+            head = list1;
+            list1 = list1.next;
+        }
+        ListNode tmp = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                tmp.next = list2;
+                list2 = list2.next;
+            } else {
+                tmp.next = list1;
+                list1 = list1.next;
+            }
+            tmp = tmp.next;
+        }
+
+        if (list1 != null) {
+            tmp.next = list1;
+        }
+        if (list2 != null) {
+            tmp.next = list2;
+        }
+
+        return head;
+    }
+}
