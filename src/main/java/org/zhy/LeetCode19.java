445dfacfc3edac6f0175180c53f8c84db1727671
ColinZhy <384414328@qq.com>
2023-08-19 21:13:07 +0800

    LeetCode19 删除链表的倒数第 N 个结点

--- src/main/java/org/zhy/LeetCode19.java
+++ src/main/java/org/zhy/LeetCode19.java
@@ -6,7 +6,7 @@ import org.zhy.structure.ListNode;
  * 19. 删除链表的倒数第 N 个结点
  * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
  */
-public class ListCode19 {
+public class LeetCode19 {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
         // 快慢指针
