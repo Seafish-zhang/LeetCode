9f580a73ffc42923e1685ab6358344285174bc26
ColinZhy <384414328@qq.com>
2023-08-19 14:43:18 +0800

    LeetCode2 两数相加

--- src/main/java/org/zhy/LeetCode2.java
+++ src/main/java/org/zhy/LeetCode2.java
@@ -1,5 +1,7 @@
 package org.zhy;
 
+import org.zhy.structure.ListNode;
+
 /**
  * 2. 两数相加
  * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
@@ -43,20 +45,5 @@ public class LeetCode2 {
     /**
      * 单链表结构
      */
-    public static class ListNode {
-        int val;
-        ListNode next;
 
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 }
