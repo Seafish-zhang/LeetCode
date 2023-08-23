f0f8096126858f8420fd22b6c767fdacd9bb898c
ColinZhy <384414328@qq.com>
2023-08-23 21:04:38 +0800

    LeetCode70 爬楼梯

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode70.java
@@ -0,0 +1,20 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 70. 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ */
+public class LeetCode70 {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[n] = dp[n - 2] + dp[n - 1];
+        }
+        return dp[n];
+    }
+
+}
