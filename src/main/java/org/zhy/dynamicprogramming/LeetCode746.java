4af0919d756ff83090c6628e1480ac2bd227f4c3
ColinZhy <384414328@qq.com>
2023-08-23 21:34:01 +0800

    LeetCode746 使用最小花费爬楼梯

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode746.java
@@ -0,0 +1,25 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 746. 使用最小花费爬楼梯
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ */
+public class LeetCode746 {
+
+    public int minCostClimbingStairs(int[] cost) {
+        int length = cost.length;
+        if (length < 2) {
+            return 0;
+        }
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[length];
+    }
+}
