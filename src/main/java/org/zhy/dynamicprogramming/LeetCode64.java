d83df8001e97230e771e176096342cf80a84d7bb
ColinZhy <384414328@qq.com>
2023-08-24 19:08:15 +0800

    LeetCode64 最小路径和

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode64.java
@@ -0,0 +1,29 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 64. 最小路径和
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ */
+public class LeetCode64 {
+
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
