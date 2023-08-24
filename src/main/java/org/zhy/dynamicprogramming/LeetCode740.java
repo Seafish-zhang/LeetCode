781bc4176dda397c3b2b41b7a7b6ac8e827fbdeb
ColinZhy <384414328@qq.com>
2023-08-24 19:06:38 +0800

    LeetCode740 删除并获得点数

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode740.java
@@ -0,0 +1,33 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 740. 删除并获得点数
+ * 给你一个整数数组 nums ，你可以对它进行一些操作。
+ * 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i] + 1 的元素。
+ * 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+ */
+public class LeetCode740 {
+
+    public int deleteAndEarn(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int[] count = new int[max + 1];
+        for (int num : nums) {
+            count[num]++;
+        }
+        int[] dp = new int[max + 1];
+        dp[1] = count[1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + count[i] * i);
+        }
+        return dp[max];
+    }
+}
