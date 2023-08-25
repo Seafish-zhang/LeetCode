79d19a65e3e7ce6eb6100404fff5aa5908320fcf
ColinZhy <384414328@qq.com>
2023-08-25 18:46:28 +0800

    LeetCode221 最大正方形

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode221.java
@@ -0,0 +1,25 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 221. 最大正方形
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ */
+public class LeetCode221 {
+
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int max = 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i + 1][j + 1] = Math.min(Math.min(dp[i + 1][j], dp[i][j + 1]), dp[i][j]) + 1;
+                    max = Math.max(max, dp[i + 1][j + 1]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
