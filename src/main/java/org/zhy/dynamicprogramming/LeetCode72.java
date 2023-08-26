859abd3a12ef46b03636c86df56a6080c9e64b52
ColinZhy <384414328@qq.com>
2023-08-26 22:25:40 +0800

    LeetCode72 编辑距离

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode72.java
@@ -0,0 +1,45 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 72. 编辑距离
+ * 给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+ * 你可以对一个单词进行如下三种操作：
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ */
+public class LeetCode72 {
+
+    public int minDistance(String word1, String word2) {
+        if (word1 == null && word2 == null) {
+            return 0;
+        }
+        if (word1 == null || word1.isEmpty()) {
+            return word2.length();
+        }
+        if (word2 == null || word2.isEmpty()) {
+            return word1.length();
+        }
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
