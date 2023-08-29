76842ad8e680a60f4e7e53f6a7e81c6ab6a7abaa
ColinZhy <384414328@qq.com>
2023-08-29 20:20:52 +0800

    LeetCode189 轮转数组

---
+++ src/main/java/org/zhy/thousand/hundred/LeetCode189.java
@@ -0,0 +1,37 @@
+package org.zhy.thousand.hundred;
+
+/**
+ * 189. 轮转数组
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ */
+public class LeetCode189 {
+
+    public void rotate(int[] nums, int k) {
+        int tmp = 0;
+        for (int i = 0; i < k; i++) {
+            tmp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = tmp;
+        }
+    }
+
+
+    public void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
