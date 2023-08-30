5da16a5855d15cc5f7ddf20c05ea42e857f29d7e
ColinZhy <384414328@qq.com>
2023-08-31 00:44:37 +0800

    LeetCode35 搜索插入位置

---
+++ src/main/java/org/zhy/thousand/hundred/LeetCode35.java
@@ -0,0 +1,35 @@
+package org.zhy.thousand.hundred;
+
+/**
+ * 35. 搜索插入位置
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ */
+public class LeetCode35 {
+
+    public static void main(String[] args) {
+        new LeetCode35().searchInsert(new int[]{1, 3, 5, 6}, 2);
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
