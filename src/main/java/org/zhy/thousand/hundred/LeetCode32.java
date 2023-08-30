892ae44bf9632710e75d72bc42aa0df119482295
ColinZhy <384414328@qq.com>
2023-08-30 21:25:08 +0800

    LeetCode32 最长有效括号

---
+++ src/main/java/org/zhy/thousand/hundred/LeetCode32.java
@@ -0,0 +1,27 @@
+package org.zhy.thousand.hundred;
+
+/**
+ * 32. 最长有效括号
+ * 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ */
+public class LeetCode32 {
+
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int max = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+}
