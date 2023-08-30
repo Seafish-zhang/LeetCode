e307ce2fbd23e28ea1bbc35837087037f1046ecf
ColinZhy <384414328@qq.com>
2023-08-30 21:29:16 +0800

    LeetCode121 买卖股票的最佳时机

---
+++ src/main/java/org/zhy/thousand/hundred/LeetCode121.java
@@ -0,0 +1,21 @@
+package org.zhy.thousand.hundred;
+
+/**
+ * 121. 买卖股票的最佳时机
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+ * 设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ */
+public class LeetCode121 {
+
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int price : prices) {
+            min = Math.min(min, price);
+            max = Math.max(max, price - min);
+        }
+        return max;
+    }
+}
