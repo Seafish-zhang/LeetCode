8359eb3577488c4ee0b953f601efae053742dbdb
ColinZhy <384414328@qq.com>
2023-08-30 21:32:21 +0800

    LeetCode300 最长递增子序列

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode300.java
@@ -0,0 +1,28 @@
+package org.zhy.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * 300. 最长递增子序列
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+ * 例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ */
+public class LeetCode300 {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
