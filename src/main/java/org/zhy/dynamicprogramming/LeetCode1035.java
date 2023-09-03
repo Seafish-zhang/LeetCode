4b382c93568770cade49cff594004d2db424025b
ColinZhy <384414328@qq.com>
2023-09-04 07:01:14 +0800

    LeetCode1035. 不相交的线

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode1035.java
@@ -0,0 +1,28 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 1035. 不相交的线
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足满足：
+ * nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ */
+public class LeetCode1035 {
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                //下面是递推公式
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+        return dp[m][n];
+    }
+}
