bbe5eb6f520ad89c6dbf4bb849188ea91b5c074a
ColinZhy <384414328@qq.com>
2023-09-04 07:11:10 +0800

    LeetCode1312. 让字符串成为回文串的最少插入次数

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode1312.java
@@ -0,0 +1,25 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 1312. 让字符串成为回文串的最少插入次数
+ * 给你一个字符串 s ，每一次操作你都可以在字符串的任意位置插入任意字符。
+ * 请你返回让 s 成为回文串的 最少操作次数 。
+ * 「回文串」是正读和反读都相同的字符串。
+ */
+public class LeetCode1312 {
+
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
