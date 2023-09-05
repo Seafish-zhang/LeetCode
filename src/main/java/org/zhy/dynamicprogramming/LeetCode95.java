7a65149d17863f32aac982b4b07d2d6eeeb9ddaa
ColinZhy <384414328@qq.com>
2023-09-06 07:18:56 +0800

    LeetCode95. 不同的二叉搜索树 II

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode95.java
@@ -0,0 +1,67 @@
+package org.zhy.dynamicprogramming;
+
+import org.zhy.structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 95. 不同的二叉搜索树 II
+ * 给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。
+ * 可以按 任意顺序 返回答案。
+ */
+public class LeetCode95 {
+
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> pre = new ArrayList<>();
+        if (n == 0) {
+            return pre;
+        }
+        pre.add(null);
+        //每次增加一个数字
+        for (int i = 1; i <= n; i++) {
+            List<TreeNode> cur = new ArrayList<>();
+            //遍历之前的所有解
+            for (TreeNode root : pre) {
+                //插入到根节点
+                TreeNode insert = new TreeNode(i);
+                insert.left = root;
+                cur.add(insert);
+                //插入到右孩子，右孩子的右孩子...最多找 n 次孩子
+                for (int j = 0; j <= n; j++) {
+                    TreeNode root_copy = treeCopy(root); //复制当前的树
+                    TreeNode right = root_copy; //找到要插入右孩子的位置
+                    int k = 0;
+                    //遍历 j 次找右孩子
+                    for (; k < j; k++) {
+                        if (right == null) break;
+                        right = right.right;
+                    }
+                    //到达 null 提前结束
+                    if (right == null) break;
+                    //保存当前右孩子的位置的子树作为插入节点的左孩子
+                    TreeNode rightTree = right.right;
+                    insert = new TreeNode(i);
+                    right.right = insert; //右孩子是插入的节点
+                    insert.left = rightTree; //插入节点的左孩子更新为插入位置之前的子树
+                    //加入结果中
+                    cur.add(root_copy);
+                }
+            }
+            pre = cur;
+
+        }
+        return pre;
+    }
+
+
+    private TreeNode treeCopy(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = treeCopy(root.left);
+        newRoot.right = treeCopy(root.right);
+        return newRoot;
+    }
+}
