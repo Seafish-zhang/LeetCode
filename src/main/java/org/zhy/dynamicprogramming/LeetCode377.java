5ac2854cf6b3f40e972f795519fd3b554abb8416
ColinZhy <384414328@qq.com>
2023-09-07 00:42:03 +0800

    LeetCode377. 组合总和 Ⅳ

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode377.java
@@ -0,0 +1,24 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 377. 组合总和 Ⅳ
+ * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。
+ * 请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+ * 题目数据保证答案符合 32 位整数范围。
+ */
+public class LeetCode377 {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int n : nums) {
+                if (i >= n) {
+                    dp[i] += dp[i - n];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
+
