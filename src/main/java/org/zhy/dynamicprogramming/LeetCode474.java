7da0f300715f244d887ed5bb450cc779d74e0509
ColinZhy <384414328@qq.com>
2023-09-07 00:53:14 +0800

    LeetCode474. 一和零

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode474.java
@@ -0,0 +1,27 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 474. 一和零
+ * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+ * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+ * 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+ */
+public class LeetCode474 {
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (String str : strs) {
+            int oneNum = 0, zeroNum = 0;
+            for (char c : str.toCharArray()) {
+                if (c == '0') zeroNum++;
+                else oneNum++;
+            }
+            for (int i = m; i >= zeroNum; i--) {
+                for (int j = n; j >= oneNum; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
