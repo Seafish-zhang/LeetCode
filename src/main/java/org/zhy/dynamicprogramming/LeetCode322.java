0a6ddbecf961cc0a8cee50b1d6e33924e7fc0f0e
ColinZhy <384414328@qq.com>
2023-09-07 18:57:46 +0800

    LeetCode322. 零钱兑换

---
+++ src/main/java/org/zhy/dynamicprogramming/LeetCode322.java
@@ -0,0 +1,28 @@
+package org.zhy.dynamicprogramming;
+
+/**
+ * 322. 零钱兑换
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * 你可以认为每种硬币的数量是无限的。
+ */
+public class LeetCode322 {
+
+    public int coinChange(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] < min) {
+                    min = dp[i - coin] + 1;
+                }
+            }
+            dp[i] = min;
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
