de1389e86d56d94c3190331356334d45f8423a40
ColinZhy <384414328@qq.com>
2023-09-28 00:04:42 +0800

    LeetCode2582. 递枕头

---
+++ src/main/java/org/zhy/daily/LeetCode2582.java
@@ -0,0 +1,17 @@
+package org.zhy.daily;
+
+/**
+ * 2582. 递枕头
+ * n 个人站成一排，按从 1 到 n 编号。
+ * 最初，排在队首的第一个人拿着一个枕头。每秒钟，拿着枕头的人会将枕头传递给队伍中的下一个人。
+ * 一旦枕头到达队首或队尾，传递方向就会改变，队伍会继续沿相反方向传递枕头。
+ * 例如，当枕头到达第 n 个人时，TA 会将枕头传递给第 n - 1 个人，然后传递给第 n - 2 个人，依此类推。
+ * 给你两个正整数 n 和 time ，返回 time 秒后拿着枕头的人的编号。
+ */
+public class LeetCode2582 {
+
+    public int passThePillow(int n, int time) {
+        int t = time % (n - 1);
+        return time / (n - 1) % 2 > 0 ? n - t : 1 + t;
+    }
+}
