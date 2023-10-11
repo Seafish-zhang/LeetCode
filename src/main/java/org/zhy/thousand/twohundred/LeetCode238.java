3df4ab9db92f6492872b64c9fa4c973436a9367f
ColinZhy <384414328@qq.com>
2023-10-11 22:25:18 +0800

    LeetCode238. 除自身以外数组的乘积

---
+++ src/main/java/org/zhy/thousand/twohundred/LeetCode238.java
@@ -0,0 +1,27 @@
+package org.zhy.thousand.twohundred;
+
+/**
+ * 238. 除自身以外数组的乘积
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+ * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+ * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ */
+public class LeetCode238 {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int len = nums.length;
+        if (len == 0) return new int[0];
+        int[] ans = new int[len];
+        ans[0] = 1;
+        int tmp = 1;
+        for (int i = 1; i < len; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            tmp *= nums[i + 1];
+            ans[i] *= tmp;
+        }
+        return ans;
+    }
+}
