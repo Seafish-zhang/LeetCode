d9d0bd6d0c549243774a4bd699796d8607ef000a
ColinZhy <384414328@qq.com>
2023-10-12 20:14:32 +0800

    LeetCode135. 分发糖果

---
+++ src/main/java/org/zhy/thousand/hundred/LeetCode135.java
@@ -0,0 +1,33 @@
+package org.zhy.thousand.hundred;
+
+import java.util.Arrays;
+
+/**
+ * 135. 分发糖果
+ * n 个孩子站成一排。给你一个整数数组 ratings 表示每个孩子的评分。
+ * 你需要按照以下要求，给这些孩子分发糖果：
+ * 每个孩子至少分配到 1 个糖果。
+ * 相邻两个孩子评分更高的孩子会获得更多的糖果。
+ * 请你给每个孩子分发糖果，计算并返回需要准备的 最少糖果数目 。
+ */
+public class LeetCode135 {
+
+    public static void main(String[] args) {
+        new LeetCode135().candy(new int[]{1, 2, 2});
+    }
+
+    public int candy(int[] ratings) {
+        int[] left = new int[ratings.length];
+        int[] right = new int[ratings.length];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        for(int i = 1; i < ratings.length; i++)
+            if(ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+        int count = left[ratings.length - 1];
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i + 1]) right[i] = right[i + 1] + 1;
+            count += Math.max(left[i], right[i]);
+        }
+        return count;
+    }
+}
