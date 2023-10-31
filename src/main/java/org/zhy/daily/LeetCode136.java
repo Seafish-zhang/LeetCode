ca8e954b1514392515ede8e4af60e95272aaa257
ColinZhy <384414328@qq.com>
2023-11-01 07:34:29 +0800

    LeetCode136. 只出现一次的数字

---
+++ src/main/java/org/zhy/daily/LeetCode136.java
@@ -0,0 +1,18 @@
+package org.zhy.daily;
+
+/**
+ * 136. 只出现一次的数字
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+ */
+
+public class LeetCode136 {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
